1259c487d07c4277a577e24ac855118226490222
asnefedov <dev.nefedov@gmail.com>
2023-01-22 10:45:05 +0300

    Changed the logic of data filtering, the emphasis on the date and time of the transaction, not the status

--- bot/utils/csv_reader.py
+++ bot/utils/csv_reader.py
@@ -1,5 +1,6 @@
-from typing import Any
+from typing import Any, Union
 
+import datetime as dt
 import numpy as np
 import pandas as pd
 from pandas.core.frame import DataFrame
@@ -8,26 +9,35 @@ from pandas.core.frame import DataFrame
 class CsvReader:
     def __init__(self, file):
         self.file = file
-        self.dictionary = ['Описание', 'Дата', 'Сумма', 'Категория', 'Счет', 'Статус', 'Время']
+        self.dictionary = ['Описание', 'Дата', 'Сумма', 'Категория', 'Счет', 'Время']
 
-    def read_csv_file(self) -> DataFrame:
+    def read_csv_file(self) -> Union[DataFrame, Exception]:
         """Accepts the file passed to the class for conversion to DataFrame"""
         try:
             return pd.read_csv(self.file, encoding='utf-8', sep=';')
         except UnicodeDecodeError:
             return pd.read_csv(self.file, encoding='ansi', sep=';')
         except Exception as error:
-            print(f'Произошла ошибка при чтении файла: {error}')
+            # TODO: log
+            return error
 
     @staticmethod
-    def get_headers(df: Any) -> list:
+    def get_headers(df: Any) -> Union[list, Exception]:
         """Gets a DateFrame to extract the file headers and returns them to a list to check"""
-        return df.columns.values.tolist()
+        try:
+            return df.columns.values.tolist()
+        except Exception as error:
+            # TODO: log
+            return error
 
     @staticmethod
-    def filtered_df_by_status(df: Any) -> DataFrame:
-        """Retrieves transactions from the DataFrame only with the status 'Unconfirmed'"""
-        filtered = df[(df['Статус'].isnull())]
+    def filtered_df_by_datetime(df: Any) -> DataFrame:
+        """Retrieves transactions from DataFrame with future date and time only'"""
+        datetime_now = dt.datetime.now()
+        date_str = datetime_now.strftime('%d/%m/%y')
+        time_str = datetime_now.strftime('%H:%M')
+        filtered = df[(df['Дата'] >= date_str) & (df['Время'] > time_str)]
+
         change_nan_on_none = filtered.replace({np.nan: None})
         return change_nan_on_none
 
@@ -49,4 +59,4 @@ class CsvReader:
         check = self.check_keys(CsvReader.get_headers(df))
         if check is not None:
             return f'Произошла ошибка при проверке файла. Заголовок {check} не найден в файле.'
-        return CsvReader.df_to_dict(CsvReader.filtered_df_by_status(df))
+        return CsvReader.df_to_dict(CsvReader.filtered_df_by_datetime(df))
